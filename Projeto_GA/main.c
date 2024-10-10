a358dac65d3baf859a8a734250161ff0b16601f9
Gabrielle Naiare <157031016+gabinaiare@users.noreply.github.com>
2024-10-10 15:09:02 -0300

    Update main.c

--- Projeto_GA/main.c
+++ Projeto_GA/main.c
@@ -3,56 +3,52 @@
 #include <locale.h>
 
 // Função para calcular o valor dentro da raiz quadrada
-double calcular_radicando(double x1, double y1, double z1, double x2, double y2, double z2) {
-    double deltaX = x2 - x1;
-    double deltaY = y2 - y1;
-    double deltaZ = z2 - z1;
+double calcular_radicando(int x1, int y1, int z1, int x2, int y2, int z2) {
+    int deltaX = x2 - x1;
+    int deltaY = y2 - y1;
+    int deltaZ = z2 - z1;
 
     // Mostrando os detalhes dos cálculos
     printf("Cálculo dos deltas:\n");
-    printf("deltaX = %.2lf - %.2lf = %.2lf\n", x2, x1, deltaX);
-    printf("deltaY = %.2lf - %.2lf = %.2lf\n", y2, y1, deltaY);
-    printf("deltaZ = %.2lf - %.2lf = %.2lf\n", z2, z1, deltaZ);
+    printf("deltaX = %d - %d = %d\n", x2, x1, deltaX);
+    printf("deltaY = %d - %d = %d\n", y2, y1, deltaY);
+    printf("deltaZ = %d - %d = %d\n", z2, z1, deltaZ);
 
-    double deltaX2 = pow(deltaX, 2);
-    double deltaY2 = pow(deltaY, 2);
-    double deltaZ2 = pow(deltaZ, 2);
+    int deltaX2 = pow(deltaX, 2);
+    int deltaY2 = pow(deltaY, 2);
+    int deltaZ2 = pow(deltaZ, 2);
 
     // Mostrando os detalhes dos quadrados
     printf("\nCálculo dos quadrados:\n");
-    printf("deltaX² = %.2lf² = %.2lf\n", deltaX, deltaX2);
-    printf("deltaY² = %.2lf² = %.2lf\n", deltaY, deltaY2);
-    printf("deltaZ² = %.2lf² = %.2lf\n", deltaZ, deltaZ2);
+    printf("deltaX² = %d² = %d\n", deltaX, deltaX2);
+    printf("deltaY² = %d² = %d\n", deltaY, deltaY2);
+    printf("deltaZ² = %d² = %d\n", deltaZ, deltaZ2);
 
     // Soma dos quadrados
-    double soma = deltaX2 + deltaY2 + deltaZ2;
+    int soma = deltaX2 + deltaY2 + deltaZ2;
 
     // Mostrando a soma dos quadrados
     printf("\nSoma dos quadrados:\n");
-    printf("%.2lf + %.2lf + %.2lf = %.2lf\n", deltaX2, deltaY2, deltaZ2, soma);
+    printf("%d + %d + %d = %d\n", deltaX2, deltaY2, deltaZ2, soma);
 
     return soma; // Este é o valor dentro da raiz
 }
 
 int main() {
-    setlocale (LC_ALL, "Portuguese");
-    // Coordenadas do primeiro caso
-    double x1_1 = -2, y1_1 = 0, z1_1 = 1;
-    double x2_1 = 1, y2_1 = -3, z2_1 = 2;
-    
-    // Coordenadas do segundo caso
-    double x1_2 = 1, y1_2 = 0, z1_2 = 1;
-    double x2_2 = 2, y2_2 = -1, z2_2 = 0;
-    
-    // Calcula o radicando para o primeiro caso
-    printf("Primeiro caso:\n");
-    double radicando1 = calcular_radicando(x1_1, y1_1, z1_1, x2_1, y2_1, z2_1);
-    printf("A distância entre P1 e P2 no primeiro caso é: raiz(%.0lf)\n", radicando1);
+    setlocale(LC_ALL, "Portuguese");
 
-    // Calcula o radicando para o segundo caso
-    printf("\nSegundo caso:\n");
-    double radicando2 = calcular_radicando(x1_2, y1_2, z1_2, x2_2, y2_2, z2_2);
-    printf("A distância entre P1 e P2 no segundo caso é: raiz(%.0lf)\n", radicando2);
+    int x1, y1, z1;
+    int x2, y2, z2;
+
+    // Solicitando os valores das coordenadas ao usuário
+    printf("Insira as coordenadas do primeiro ponto (x1, y1, z1):\n");
+    scanf("%d %d %d", &x1, &y1, &z1);
+
+    printf("Insira as coordenadas do segundo ponto (x2, y2, z2):\n");
+    scanf("%d %d %d", &x2, &y2, &z2);
+
+    double radicando = calcular_radicando(x1, y1, z1, x2, y2, z2);
+    printf("\nA distância entre P1 e P2 é: raiz(%.0lf)\n", radicando);
 
     return 0;
 }
